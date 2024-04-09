1ef125e56ebf44c00ce0e8a9cbb6869655583509
DosSantosDev2022 <dossantosdevoficial@gmail.com>
2024-04-08 22:45:47 -0300

    feat: iniciando página about, criando seção welcome

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,7 +1,22 @@
+import coverHero from '../assets/about/aboutHero.webp'
+import { welcome } from '../configs/aboutdata.json'
+
 export function AboutPage() {
+  const welcomeData = welcome
   return (
-    <div>
-      <h1>About page</h1>
-    </div>
+    <main>
+      <section className="lg:px-[162px] px-4 mt-[100px] bg-absolute_white ">
+        <img src={coverHero} alt="" />
+
+        <div className="lg:p-24 p-[30px] flex flex-col gap-[50px] ">
+          <h1 className="text-GreyShades_900 text-5xl leading-[72px] font-normal   ">
+            {welcomeData.title}{' '}
+          </h1>
+          <p className="text-lg leading-[27px] font-normal text-GreyShades_800 ">
+            {welcomeData.content}{' '}
+          </p>
+        </div>
+      </section>
+    </main>
   )
 }
