3de25cff99fdfe34425cfb06ea1d2a9d175ed99b
DosSantosDev2022 <dossantosdevoficial@gmail.com>
2024-04-13 14:39:17 -0300

    feat: iniciando página process

--- src/pages/process.tsx
+++ src/pages/process.tsx
@@ -1,7 +1,24 @@
+import { Process } from '../components/icons/processLogo'
+import { process } from '../configs/processdata.json'
+
 export function ProcessPage() {
   return (
-    <div>
-      <h1>Process pages</h1>
-    </div>
+    <main>
+      <section className="lg:px-[162px] px-4 mt-[150px] bg-absolute_white ">
+        <div className="w-full flex flex-col gap-20">
+          <div className="flex flex-col gap-[10px] text-center">
+            <Process />
+            <h2 className="text-5xl text-GreyShades_900 font-bold leading-[72px] ">
+              {process.title}
+            </h2>
+            <span className="ext-GreyShades_800 text-lg leading-[27px] font-normal ">
+              {process.description}
+            </span>
+          </div>
+
+          <div className="grid lg:grid-cols-2 grid-cols-1 gap-7"></div>
+        </div>
+      </section>
+    </main>
   )
 }
