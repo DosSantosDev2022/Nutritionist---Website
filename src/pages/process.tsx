b545b1d4115e52b98171294c4240e3238a1c1c7c
DosSantosDev2022 <dossantosdevoficial@gmail.com>
2024-04-15 22:33:32 -0300

    feat: finalizando página process

--- src/pages/process.tsx
+++ src/pages/process.tsx
@@ -1,22 +1,70 @@
+import { Button } from '../components/button'
 import { Process } from '../components/icons/processLogo'
-import { process } from '../configs/processdata.json'
+import { process, cards, action } from '../configs/processdata.json'
 
 export function ProcessPage() {
   return (
     <main>
       <section className="lg:px-[162px] px-4 mt-[150px] bg-absolute_white ">
         <div className="w-full flex flex-col gap-20">
-          <div className="flex flex-col gap-[10px] text-center">
+          <div className="flex flex-col gap-[10px] text-center justify-center items-center">
             <Process />
             <h2 className="text-5xl text-GreyShades_900 font-bold leading-[72px] ">
               {process.title}
             </h2>
-            <span className="ext-GreyShades_800 text-lg leading-[27px] font-normal ">
+            <span className="text-GreyShades_800 text-lg leading-[27px] font-normal ">
               {process.description}
             </span>
           </div>
 
-          <div className="grid lg:grid-cols-2 grid-cols-1 gap-7"></div>
+          <section className="fle flex-col gap-20 mt-[100px] ">
+            <div className=" text-center space-y-2 ">
+              <h3 className="text-5xl text-GreyShades_900 font-bold leading-[72px] ">
+                {process.subtitle}{' '}
+              </h3>
+              <p className="text-GreyShades_800 text-lg leading-[27px] font-normal ">
+                {process.subDescription}{' '}
+              </p>
+            </div>
+            <div className="grid lg:grid-cols-2 grid-cols-1 gap-12">
+              {cards.map((card) => (
+                <div key={card.number} className="h-[318px] ">
+                  <span className="text-[9.375rem] font-bold  text-DarkGreen_300 relative ">
+                    {card.number}
+                  </span>
+
+                  <div className="relative -top-24  flex flex-col items-start self-stretch gap-3 p-[30px] lg:h-[234px] h-[274px] bg-GreenShades_300 rounded-md ">
+                    <div className="flex items-center justify-start gap-[14px]  w-full ">
+                      <div className="bg-GreenShades_700 lg:p-4 p-[14px] rounded-lg lg:w-[66px] w-[54px] h-[54px] flex items-center justify-center ">
+                        <img src={card.icon} alt={card.title} className="w-7" />
+                      </div>
+
+                      <h4 className="lg:text-2xl text-md font-semibold  leading-[21px]  text-GreyShades_900">
+                        {card.title}
+                      </h4>
+                    </div>
+                    <p className="text-lg leading-[21px] flex items-center font-medium text-GreyShades_700 w-full  lg:h-[105px] ">
+                      {card.description}{' '}
+                    </p>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </section>
+
+          <div className="w-full flex flex-col lg:flex-row justify-between items-center gap-20 mt-[100px] rounded-md p-4 lg:p-20 bg-GreenShades_200 border border-GreenShades_300">
+            <div className="flex flex-col gap-[14px]  ">
+              <h4 className="text-GreyShades_900 font-bold leading-[41px] text-[2.125rem] ">
+                {action.title}
+              </h4>
+              <p className="text-lg font-medium leading-[27px] text-GreyShades_800 ">
+                {action.description}
+              </p>
+            </div>
+            <Button className=" w-[250px]  " variant="primary">
+              Faça parte
+            </Button>
+          </div>
         </div>
       </section>
     </main>
