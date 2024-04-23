5aa1e2070c4dbbf68c28766bb07fa820955cbee8
DosSantosDev2022 <dossantosdevoficial@gmail.com>
2024-04-22 23:43:57 -0300

    feat: adicionando toastContainer

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { Outlet } from 'react-router-dom'
 import { Header } from './components/layout/header'
 import { Footer } from './components/layout/footer'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/ReactToastify.css'
 
 function App() {
   return (
@@ -8,6 +10,7 @@ function App() {
       <Header />
       <Outlet />
       <Footer />
+      <ToastContainer />
     </div>
   )
 }
